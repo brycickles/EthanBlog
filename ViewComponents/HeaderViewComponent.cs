7204b933d73c622ed04756f783e9e74dee6f75cb
brycickles <56007309+brycickles@users.noreply.github.com>
2024-06-25 11:22:23 -0500

    Moved footer and header in layout to be a view component> Also makes layout more readable.

---
+++ ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EthanBlog.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent{        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() => { return View(); });
+        }
+    }
+}
