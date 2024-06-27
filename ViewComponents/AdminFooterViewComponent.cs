6082e22dd5a1c0bbb4ea5a9d03374ed64d5d7153
brycickles <56007309+brycickles@users.noreply.github.com>
2024-06-27 09:48:15 -0500

    SICKLES - added bootstrap admin page, parsed it out into components, and edited aspects of page to be more relevant.

---
+++ ViewComponents/AdminFooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EthanBlog.ViewComponents
+{
+    public class AdminFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() => { return View(); });
+        }
+
+    }   
+}
